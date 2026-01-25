9bbd1f1bee92601c1544579cc49765c9f6c6c5c6
Curtis Chen <curtischen1234@gmail.com>
2026-01-24 20:15:54 -0800

    Replace hero with team picture

--- components/HeroContentLeft.tsx
+++ components/HeroContentLeft.tsx
@@ -15,7 +15,7 @@ export function HeroContentLeft() {
   return (
     <div className={classes.hero}>
       <Overlay
-        gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
+        gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 95%)"
         opacity={1}
         zIndex={0}
       />
